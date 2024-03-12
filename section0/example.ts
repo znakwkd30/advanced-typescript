d09b5a41eff81f648bbdf4eb143d72662f022439
HyeonJun <gwakhyeonjun@gmail.com>
2024-03-12 17:58:16 +0900

    section0 문제 풀이

---
+++ section0/example.ts
@@ -0,0 +1,11 @@
+import { Equal, Expect } from "../helper";
+
+const obj = {
+    hello: 'world',
+    hello2: 'world2',
+    hello3: 'world3',
+};
+
+type TestingFramework = unknown;
+
+type tests = [Expect<Equal<TestingFramework, "hello" | "hello2" | "hello3">>];
