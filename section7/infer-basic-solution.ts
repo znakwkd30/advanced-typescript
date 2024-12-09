e6281c67600af6d2ee8db4bcbe7718264f0e16a7
HyeonJun <gwakhyeonjun@gmail.com>
2024-12-09 11:20:10 +0900

    section7 infer 소개 문제 풀이

---
+++ section7/infer-basic-solution.ts
@@ -0,0 +1,17 @@
+import { Equal, Expect } from "../helper";
+
+type GetDataValue<T> = T extends { data: infer T2 } ? T2 : never;
+
+type tests = [
+  Expect<Equal<GetDataValue<{ data: "hello" }>, "hello">>,
+  Expect<Equal<GetDataValue<{ data: { name: "hello" } }>, { name: "hello" }>>,
+  Expect<
+    Equal<
+      GetDataValue<{ data: { name: "hello"; age: 20 } }>,
+      { name: "hello"; age: 20 }
+    >
+  >,
+  // Expect that if you pass in string, it
+  // should return never
+  Expect<Equal<GetDataValue<string>, never>>
+];
