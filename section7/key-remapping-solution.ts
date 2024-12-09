179e0fbed21b6f5ee7eb6d44e6548b5dfd7259c5
HyeonJun <gwakhyeonjun@gmail.com>
2024-12-09 11:10:51 +0900

    section7 조건 타입으로 Object 키 새로 만들기 문제풀이

---
+++ section7/key-remapping-solution.ts
@@ -0,0 +1,27 @@
+import { Equal, Expect } from "../helper";
+
+interface Example {
+  name: string;
+  age: number;
+  id: string;
+  organisationId: string;
+  groupId: string;
+}
+
+type OnlyIdKeys<T> = {
+  [K in keyof T as K extends `${string}${"id" | "Id"}` ? K : never]: T[K];
+};
+
+type tests = [
+  Expect<
+    Equal<
+      OnlyIdKeys<Example>,
+      {
+        id: string;
+        organisationId: string;
+        groupId: string;
+      }
+    >
+  >,
+  Expect<Equal<OnlyIdKeys<{}>, {}>>
+];
