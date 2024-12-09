ed0e7e0d3e653aeb8a36cb0e62753a6df5c147b4
HyeonJun <gwakhyeonjun@gmail.com>
2024-12-09 10:56:57 +0900

    section7 조건 타입에서 never 문제풀이

---
+++ section7/returning-never-solution.ts
@@ -0,0 +1,14 @@
+import { Equal, Expect } from "../helper";
+
+type YouSayGoodbyeAndISayHello<T> = T extends "hello"
+  ? "goodbye"
+  : T extends "goodbye"
+  ? "hello"
+  : never;
+
+type tests = [
+  Expect<Equal<YouSayGoodbyeAndISayHello<"hello">, "goodbye">>,
+  Expect<Equal<YouSayGoodbyeAndISayHello<"goodbye">, "hello">>,
+  Expect<Equal<YouSayGoodbyeAndISayHello<"alright pal">, never>>,
+  Expect<Equal<YouSayGoodbyeAndISayHello<1>, never>>
+];
---
+++ section7/returning-never.ts
@@ -0,0 +1,10 @@
+import { Equal, Expect } from "../helper";
+
+type YouSayGoodbyeAndISayHello<T> = unknown;
+
+type tests = [
+  Expect<Equal<YouSayGoodbyeAndISayHello<"hello">, "goodbye">>,
+  Expect<Equal<YouSayGoodbyeAndISayHello<"goodbye">, "hello">>,
+  Expect<Equal<YouSayGoodbyeAndISayHello<"alright pal">, never>>,
+  Expect<Equal<YouSayGoodbyeAndISayHello<1>, never>>
+];
